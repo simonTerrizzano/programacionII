78092d48aaed8da8844a4dfbc1a18dfbb614256d
simonTerrizzano <simterri25@gmail.com>
2023-04-07 11:49:25 -0300

    Cambios en Ej6.c

--- Ejercicio_6.c
+++ Ejercicio_6.c
@@ -1,14 +1,14 @@
 #include "Ejercicio_6.h"
 
 void mafiachina(int num1,int num2,int num1copia){
-    if (num1!=1)
+    if (num1>1)
     {
-        if (num2==0)
+        if (num2==0)//Si es 0 realiza los de la izquierda
         {
             printf("(-.");
             return mafiachina(num1-1,num2,num1copia);
         }
-        if (num2=1)
+        if (num2==1)//Si es 1 realiza los de la derecha
         {
             printf(".-)");
             return mafiachina(num1-1,num2,num1copia);
@@ -18,7 +18,7 @@ void mafiachina(int num1,int num2,int num1copia){
         if (num2==0)
         {
             printf("(-.-)");
-            return mafiachina(num1copia,1,num1copia);
+            return mafiachina(num1copia,1,num1);
         }
     }
 }
