a149226b8b008b24006713ddee271fde18305897
Andrés Alvarez <87197458+Cacho-Bebop@users.noreply.github.com>
2023-04-08 20:40:33 -0300

    inclusión de minúsculas.

--- Ejercicio_7andres.c
+++ Ejercicio_7andres.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 // onda_digital(char *forma_onda):la funcion mostrara en pantalla el string apuntado por forma_onda representando los valores con guiones.
-// La funcion solamente procesa los caracteres L y H mayusculas, cualquier otro caracter (incluso l y h(minusculas)) no pertenecen al dominio de la funcion. 
+// La funcion solamente procesa los caracteres L/l y H/h, cualquier otro caracter  no pertenecen al dominio de la funcion. 
 void
 onda_digital(char *forma_de_onda);
 int main()
@@ -20,11 +20,11 @@ onda_digital(char *forma_de_onda)
 {
     // Si el caracter no representara LOW o HIGH, o fuera el caracter nulo, entonces corta la funcion.
     // Si la forma de onda es LOW
-    if((*forma_de_onda) == 'L')
+    if((*forma_de_onda) == 'L' || *forma_de_onda == 'l')
     {
 	// Y la siguiente es HIGH
 	
-	if((*(forma_de_onda+1)) == 'H')
+	if((*(forma_de_onda+1)) == 'H' || *(forma_de_onda+1)) == 'h')
 	{
 	   
 	    // Imprimimos low y comienza a subir
@@ -40,9 +40,9 @@ onda_digital(char *forma_de_onda)
 	
     }
     // lo siguiente es analogo a lo expuesto anteriormente.
-    else if((*forma_de_onda) == 'H')
+    else if((*forma_de_onda) == 'H' || *(forma_de_onda+1)) == 'h')
     {
-	if((*(forma_de_onda + 1)) == 'L')
+	if((*(forma_de_onda + 1)) == 'L' || (*(forma_de_onda + 1)) == 'l')
 	{
 	    printf("%c",238);printf("|");
 	    onda_digital(forma_de_onda+1);
