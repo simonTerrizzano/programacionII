d3bb0657ce97b819b7f36730d6db4dbffd9fe222
Andrés Alvarez <87197458+Cacho-Bebop@users.noreply.github.com>
2023-04-09 17:36:07 -0300

    Añadiendo portabilidad al código

--- Ejercicio_5_main.c
+++ Ejercicio_5_main.c
@@ -1,22 +1,25 @@
 #include "Ejercicio_5.c"
 
-int main(void)
+int main()
 {
     int numero_sin_puntos;
+    char buffer[50];
 
     printf("--Numero para agregar separador de miles--");
 
-    numero_sin_puntos=ingreso_normalizado_enteros(0,999999999);
+    numero_sin_puntos=ingreso_normalizado_enteros(-999999999,999999999);
 
     char* numero_con_puntos;
 
-    itoa(numero_sin_puntos,numero_con_puntos,10);
-
-    agregar_separador_de_miles(numero_con_puntos);
+    //itoa(numero_sin_puntos,numero_con_puntos,10); // itoa no es parte del standar. Por lo tanto solo correra en
+    //bibliotecas que tengan implementaciones no standar.
+    // sprintf agrega portabilidad al codigo
+    sprintf(buffer,"%d",numero_sin_puntos);
+    agregar_separador_de_miles(buffer);
 
     printf("\n\n");
 
-    system("PAUSE");
+    exit(EXIT_SUCCESS);
 
     return 0;
 }
