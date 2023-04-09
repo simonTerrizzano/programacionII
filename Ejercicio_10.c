a6605a22f610e5438c6a2776388ea6d08e50ea97
olivieri321 <83927968+olivieri321@users.noreply.github.com>
2023-04-09 13:19:38 -0300

    Update Ejercicio_10.c

--- Ejercicio_10.c
+++ Ejercicio_10.c
@@ -17,15 +17,33 @@ Número: 20
 Bomba: 5
 explosion(20, 5) => [4 3 2 2 1 1 1 1 5]*/
 
-void explosivo(int N, int B){
-
-    if (N<=B)//Caso base si N es menos o igual que la bomba explota
-    {
-        printf(" %d ",N);
-    }
-    else{
-        explosivo((N/B),B);//Exploción N1
-        explosivo(N-(N/B),B);//Explosión N2
-
+int* explocion(int num, int bomba, int* tamanio) {
+    if (num <= bomba) {
+        //Si el numero es menor o igual a la bomba, se aloja en memoria un array el cual se le asignara el numero al array y se incrementara el tamaño para luego alojar el array en el cual se alojaran todos los int que cumplen la condicion 
+        int* arr = malloc(sizeof(int));
+        *arr = num;
+        *tamanio = 1;
+        return arr;
+    } else {
+        //se crean dos contadores del tamaño del array que devolveran las llamadas recursivas
+        int tamanio1, tamanio2;
+        //se crean los arrays que tendran los numeros resultantes de la operacion
+        int* arr1 = explocion(num / bomba, bomba, &tamanio1);
+        int* arr2 = explocion(num - (num / bomba), bomba, &tamanio2);
+        //se crea array principal
+        int* arr = malloc((tamanio1 + tamanio2) * sizeof(int));
+        // se asignaran los valores de los distintos arrays alojados en memoria al array "arr"
+        for (int i = 0; i < tamanio1; i++) {
+            arr[i] = arr1[i];
+        }
+        for (int i = 0; i < tamanio2; i++) {
+            arr[tamanio1 + i] = arr2[i];
+        }
+        // se cambiara el tamaño al de los dos array creados por las llamadas recursivas
+        *tamanio = tamanio1 + tamanio2;
+        // liberar memoria
+        free(arr1);
+        free(arr2);
+        return arr;
     }
 }
