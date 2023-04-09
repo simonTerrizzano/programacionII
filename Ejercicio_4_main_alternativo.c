30ae92158a3b5eb1265fe9938932a292eeb833a0
Andrés Alvarez <87197458+Cacho-Bebop@users.noreply.github.com>
2023-04-09 15:50:05 -0300

    EJERCICIO 4 ALTERNATIVO

---
+++ Ejercicio_4_main_alternativo.c
@@ -0,0 +1,44 @@
+#include "Ejercicio_4.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char salto_linea;
+    int numerador, denominador;
+    int denominador_cero;
+    printf("--Numerador--");
+    printf("\n\nIngrese un numerador: ");
+    scanf("%d%c", &numerador, &salto_linea);
+
+    printf("\n\n--Denominador--");
+    printf("\nIngrese un denominador (distinto de 0. Porfa ;) ): ");
+    scanf("%d%c", &denominador, &salto_linea);
+    
+    if(denominador==0)
+    {
+	printf("La division por 0 no esta definida. Porfavor intente con un numero distinto de 0\n\n");
+	exit(EXIT_FAILURE);
+    }
+    
+    // si ambos tienen el mismo signo, entonces lo muestra positivo.
+    if( (numerador < 0 || denominador < 0)  && !(denominador < 0 && numerador < 0) )
+    {
+	printf("\nResultado: -%.4f", division(abs(numerador), abs(denominador), PRECISION));
+	
+    }
+    else
+    {
+
+	printf("\nNumerador:%d\tDenominador:%d", numerador, denominador);
+	printf("\nResultado: %.4f", division(numerador, denominador, PRECISION));
+    }
+    
+    
+
+    printf("\n\n");
+
+    exit(EXIT_SUCCESS);
+
+    return 0;
+}
