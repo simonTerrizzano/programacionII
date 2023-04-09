07baa18d8ee728d6f0cb494b07315051fef54a27
Andrés Alvarez <87197458+Cacho-Bebop@users.noreply.github.com>
2023-04-09 15:16:27 -0300

    Update Ejercicio_4.h

--- Ejercicio_4.h
+++ Ejercicio_4.h
@@ -4,9 +4,9 @@
 #define Ejercicio_4
 #define PRECISION 4
 
-float division(int m, int n, int precision);
-float calcular_parte_decimal(int m, int n, int precision);
-int how_many_times(int m, int n);
+float division(unsigned int m, unsigned int n, unsigned int precision);
+float calcular_parte_decimal(unsigned int m, unsigned int n, unsigned int precision);
+int how_many_times(unsigned int m, unsigned int n);
 int recursive_power(int m, int k);
 
 #endif
