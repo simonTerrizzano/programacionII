11b22f128160d6f6f6372a96a7f148594a2145d9
Andrés Alvarez <87197458+Cacho-Bebop@users.noreply.github.com>
2023-04-09 17:13:43 -0300

    Cambios correspondientes en main ejercicio 4
    Cambio el ejercicio 4 para que compruebe si el denominador es distinto de 0. También añadí para que muestre si alguno de los 2 números es negativo

--- Ejercicio_4_main.c
+++ Ejercicio_4_main.c
@@ -5,16 +5,31 @@ int main(void)
     int numerador, denominador;
 
     printf("--Numerador--");
-    numerador=ingreso_normalizado_enteros(0,1000);
+    numerador=ingreso_normalizado_enteros(-1000,1000);
 
     printf("--Denominador--");
-    denominador=ingreso_normalizado_enteros(0,1000);
+    denominador=ingreso_normalizado_enteros(-1000,1000);
+    if(denominador==0)
+    {
+	printf("La division por 0 no esta definida. Porfavor intente con un numero distinto de 0 la proxima vez\n\n");
+	exit(EXIT_FAILURE);
+    }
+    
+    if( (numerador < 0 || denominador < 0)  && !(denominador < 0 && numerador < 0) )
+    {
+	printf("\nResultado: -%.4f", division(abs(numerador), abs(denominador), PRECISION));
+	
+    }
+    else
+    {
 
-    printf("\nResultado: %.4f", division(numerador, denominador, PRECISION));
-
+	printf("\nNumerador:%d\tDenominador:%d", numerador, denominador);
+	printf("\nResultado: %.4f", division(numerador, denominador, PRECISION));
+    }
+    
     printf("\n\n");
 
-    system("PAUSE");
+    exit(EXIT_SUCCESS);
 
     return 0;
 }
