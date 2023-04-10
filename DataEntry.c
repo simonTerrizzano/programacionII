67310c89b0646169d565771d0dc96bdc14c5ea54
olivieri321 <83927968+olivieri321@users.noreply.github.com>
2023-04-10 17:45:08 -0300

    Update DataEntry.c

--- DataEntry.c
+++ DataEntry.c
@@ -36,10 +36,10 @@ int ingreso_normalizado_enteros(int limite_inferior, int limite_superior){
     {   
         if (limite_inferior<0)
         {
-            printf("\nIngrese un número (Número entero entre %i y %i):\n",limite_inferior,limite_superior);
+            printf("\nIngrese un numero (Número entero entre %i y %i):\n",limite_inferior,limite_superior);
         }else
         {
-            printf("\nIngrese un número (Número entero positivo entre %i y %i):\n",limite_inferior,limite_superior);
+            printf("\nIngrese un numero (Número entero positivo entre %i y %i):\n",limite_inferior,limite_superior);
         }
         
         fgets(ingreso,11,stdin);
@@ -78,7 +78,7 @@ int ingreso_normalizado_enteros(int limite_inferior, int limite_superior){
             }
         }else{
             if (cadenatododigito(ingreso)!=1){//Comprueba si el ingreso es un digito
-                printf("\nError: Por favor solo ingrese números enteros positivos.\n");
+                printf("\nError: Por favor solo ingrese numeros enteros positivos.\n");
             }
             else if (ingreso_normalizado>limite_superior)//Comprueba limite superior
             {
