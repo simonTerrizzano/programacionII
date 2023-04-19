c59a5e96f90657bda3397030e7ce3befac6f1f5a
olivieri321 <83927968+olivieri321@users.noreply.github.com>
2023-04-19 11:51:06 -0300

    Create Ejercicio_6.h

---
+++ TP2_TAD_Listas/Ejercicio_6.h
@@ -0,0 +1,9 @@
+#include "listas_cursores.c"
+//#include "listas_punteros.c"
+//#include "listas_arreglos.c"
+#include "tipo_elemento.c"
+#include "DataEntry.c"
+
+bool esSubLista(Lista lista1, Lista lista2);
+int generarValoresLista(Lista lista, int num);
+int ingresoValorLista(Lista lista);
