ce0da7350c9ed5d1f860cae79f41286d168d2ee0
olivieri321 <83927968+olivieri321@users.noreply.github.com>
2023-04-18 22:56:29 -0300

    Update tipo_elemento.c

--- TP2_Listas/tipo_elemento.c
+++ TP2_Listas/tipo_elemento.c
@@ -1,14 +1,26 @@
-#include "tipo_elemento.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "tipo_elemento.h"
 
-TipoElemento te_crear (int clave) {
-    TipoElemento te = (TipoElemento) malloc(sizeof (struct TipoElementoRep));
+// te_crear: recibe un entero el cual sera la clave del elemento creado.
+// return: devuelve un puntero al elemento
+TipoElemento te_crear(int clave)
+{
+    // se crea dinamicamente un tipo elemento
+    TipoElemento te = (TipoElemento) malloc(sizeof(TipoElemento));
+    // se le asigna una clave
     te->clave = clave;
     te->valor = NULL;
     return te;
+
 }
-TipoElemento te_crear_con_valor (int clave, void* valor) {
-    TipoElemento te = (TipoElemento) malloc(sizeof (struct TipoElementoRep));
+
+// te_crear: recibe un entero el cual sera la clave del elemento creado y recibe la dirección del dato el cual debera apuntar el te creado.
+// return: devuelve un puntero al elemento
+TipoElemento te_crear_con_valor(int clave, void *valor)
+{
+    TipoElemento te = (TipoElemento) malloc(sizeof(TipoElemento));
     te->clave = clave;
     te->valor = valor;
     return te;
