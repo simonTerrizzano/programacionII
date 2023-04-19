9b90f15ae84fc34aa3f70c8b97d1ac05c5f8ec2a
olivieri321 <83927968+olivieri321@users.noreply.github.com>
2023-04-19 12:45:33 -0300

    Create Ejercicio_3.h

---
+++ TP2_TAD_Listas/Ejercicio_3.h
@@ -0,0 +1,8 @@
+#include "DataEntry.c"
+#include "listas_cursores.c"
+#include "tipo_elemento.c"
+
+
+bool multiploLista(Lista lista1, Lista lista2);
+int ingresoValorLista(Lista lista);
+void generarValoresLista(Lista lista, int num);
