1730a1284b27eb0e2ab2dd30e8c52650e5903536
cacho <Jandres811@outlook.com.ar>
2023-04-29 22:32:19 -0300

    p5

--- TP3_PILAS/punto5.c
+++ TP3_PILAS/punto5.c
@@ -19,3 +19,5 @@ Pila p_invertida(Pila p_src)
 
 
 }
+
+//COMPLEJIDAD ALGORITMICA: COMPLEJIDAD ALGORITMICA ES DE O(N). Ya que la cantidad de lineas a ejecutar es proporcional a N cantidad de elementos de la pila. El crecimiento es lineal, y el peor caso sera que se ejecute N veces. Luego tenemos p_devolver(), que tambien la cantidad de lineas a ejecutar es proporcional a la cantidad de elementos. Por lo tanto el crecimiento es al rededor de 2n en el peor caso.
