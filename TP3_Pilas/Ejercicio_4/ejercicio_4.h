7ef78252e2b0c9180b72f50f89612c8aff03a18a
Simon Terrizzano <97785846+simonTerrizzano@users.noreply.github.com>
2023-05-08 17:06:51 -0300

    Update ejercicio_4.h

--- TP3_Pilas/Ejercicio_4/ejercicio_4.h
+++ TP3_Pilas/Ejercicio_4/ejercicio_4.h
@@ -4,8 +4,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include "C:\Users\PC\Documents\ProgramacionII\programacionII\programacionII\TP3_Pilas\Implementaciones_TAD_Pilas\pilas.h"
-#include "C:\Users\PC\Documents\ProgramacionII\programacionII\programacionII\DataEntry\DataEntry.h"
+#include "TP3_Pilas\Implementaciones_TAD_Pilas\pilas.h"
+
+#include "DataEntry\DataEntry.h"
+
+
+
 // p_numero_a_base: Convierte un Numero_Decimal a cierta Base y lo muestra en pantalla.
 // Base debe de ser un numero entre 2 y 16.
 void p_numero_a_base(int n, unsigned int b);
