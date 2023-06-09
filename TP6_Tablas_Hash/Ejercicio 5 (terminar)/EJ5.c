f9fd842073671ea3176c4ee6bcdc75cbfc8ce228
martinweisbek <mar02weis@gmail.com>
2023-06-09 11:41:47 -0300

    Problemas en el calculo del tiempo. Siempre da 0

--- TP6_Tablas_Hash/Ejercicio 5 (terminar)/EJ5.c
+++ TP6_Tablas_Hash/Ejercicio 5 (terminar)/EJ5.c
@@ -4,9 +4,36 @@
 #include <sys/time.h>
 #include <math.h>
 
-#include "tabla_hash.h"
-#include "arbol_avl.h"
-#include "Validacion_datos.h"
+#include "tabla_hash.h" //Incluir TAD tabla hash
+#include "arbol_avl.h" //Incluir TAD arbol AVL
+
+
+//=======================================
+bool validarEntero(char* cadena)
+{
+    int i;
+    bool esNumero = false;
+    int longitud = strlen(cadena);
+
+    if (cadena[0] == '-') i = 1;
+    else i = 0;
+
+    for (i; i < longitud; i++)
+    {
+        //Valido que el input sea un NUMERO
+        if (cadena[i] >= 48 && cadena[i] <=57)
+        {
+            if (cadena[i] != ' ') esNumero = true;
+            else return false;
+            
+        }
+        else return false;
+    }
+
+    return esNumero;
+}
+//=======================================
+
 
 
 /*5. Realizar una comparación de los tiempos de accesos a las claves entre un árbol AVL y 
@@ -23,10 +50,7 @@ int funcionHash(int clave)
 }
 
 
-/*
- @param intervalo minimo
- @param intermalo maximo 
-*/
+
 TablaHash CargarTH(int min, int max, int repeticiones)
 {
     TablaHash th = th_crear(2003,&funcionHash);
@@ -50,52 +74,124 @@ ArbolAVL CargarAVL(int min, int max, int repeticiones)
     for (i = 0; i < repeticiones; i++)
     {
         numero = rand() % ((max+1)- min) + min;
-        avl_insertar(avl,te_crear(numero));
+        avl_insertar(avl,te_crear(numero)); 
     }
+    return avl;
 }
 
 
 void Tiempo_Busqueda(TablaHash th, ArbolAVL avl, int clave)
 {
     int i;
-    struct timeval comienzo,final;
-    gettimeofday(&comienzo,NULL);
+    struct timeval comienzo_th,final_th;
+    struct timeval comienzo_avl,final_avl;
 
-    for (i = 0; i < 1000000; i++)
-    {
-        th_recuperar(th,432);
-        th_recuperar(th,254);
-        th_recuperar(th,2);
-        th_recuperar(th,762);       
-    }
+    printf("===============================\n");
 
+    //Comienzo el contador
+    gettimeofday(&comienzo_th,NULL);
+    th_recuperar(th,clave);
+    gettimeofday(&final_th,NULL);
+    long microsegundos_th = final_th.tv_usec - comienzo_th.tv_usec;
+    printf("Tiempo tabla hash (microsegundos) = %Lf\n", (double) microsegundos_th*1e-6);
+
+
+    gettimeofday(&comienzo_avl,NULL);
+    avl_buscar(avl,clave);
+    gettimeofday(&final_avl,NULL);
+    long microsegundos_avl = final_avl.tv_usec - comienzo_avl.tv_usec;
+    printf("Tiempo arbol AVL (microsegundos) = %Lf\n", (double) microsegundos_avl*1e-6);
     
-    gettimeofday(&final,NULL);
-    long microsegundos = final.tv_usec - comienzo.tv_usec;
-    printf("Tiempo tabla hash = %ld", (final.tv_usec - comienzo.tv_usec));
 }
 
 
 int main()
 {
 
-    TablaHash th = th_crear(2003,&funcionHash);
-    ArbolAVL avl = avl_crear();
+    TablaHash th;
+    ArbolAVL avl;
+    bool salir = false;
+    int cantidad_claves, min,max, repeticiones,clave,i;
+    char* input = malloc(sizeof(char));
 
-    th_insertar(th,te_crear(2));
-    th_insertar(th,te_crear(254));
-    th_insertar(th,te_crear(762));
-    th_insertar(th,te_crear(432));
-    th_insertar(th,te_crear(26565));
-    th_insertar(th,te_crear(58678));
+    srand(time(NULL));
 
-    
+    while (!salir)
+    {
+        //Pregunto cuantas claves hay que generar
+        while (true)
+        {
+            printf("Ingrese la cantidad de claves que va a contener la tabla hash y el arbol AVL (1 a 2000)\n");
+            printf("Cantidad de claves:");
+            gets(input);
+            if (validarEntero(input))
+            {
+                cantidad_claves = strtol(input,NULL,10);
+                if (cantidad_claves >= 1 && cantidad_claves <= 2000) input[0] = '\0'; break;     
+            }
 
- 
-    Tiempo_Busqueda(th,avl,432);
+            printf("Se ingreso un dato incorrecto. Vuelva a intentarlo\n");
+        }
+
+        //Pido el minimo valor
+        while (true)
+        {
+            printf("Ingrese el minimo valor a generar: ");
+            gets(input);
+            if (validarEntero(input))
+            {
+                min = strtol(input,NULL,10);
+                if (min > 0 && min <= 2000) input[0]; break;
+            }
+            printf("Se ingreso un dato incorrecto. Vuelva a intentarlo\n");
+        }
+
+        //Pido el maximo valor
+        while (true)
+        {
+            printf("Ingrese el maximo valor a generar: ");
+            gets(input);
+            if (validarEntero(input))
+            {
+                max = strtol(input,NULL,10);
+                if (max > min && max <= 2000) input[0]; break;
+            }
+            printf("Se ingreso un dato incorrecto. Vuelva a intentarlo\n");
+        }
+
+        //Pido cantidad de repeticiones
+        while (true)
+        {
+            printf("Ingrese la cantidad de veces que se va a repetir el proceso (1 a 5000):");
+            gets(input);
+            if (validarEntero(input))
+            {
+                repeticiones = strtol(input,NULL,10);
+                if (repeticiones >= 1 && repeticiones <= 5000) input[0]; break;
+                printf("Se ingreso un dato incorrecto. Vuelva a intentarlo\n");
+            }
+        }
+
+
+        th = CargarTH(min,max,repeticiones);
+        avl = CargarAVL(min,max,repeticiones);
+
+        for (i = 0; i < repeticiones; i++)
+        {
+            clave = rand() % ((max+1)- min) + min;
+            Tiempo_Busqueda(th,avl,clave);
+        }
+
+        //Consulo si quiere salir del programa
+        printf("\nIngrese X si quiere salir o enter para continuar...\n");
+        gets(input);
+        if ((input[0] == 'x' || input[0] == 'X') && (strlen(input) == 1)) salir = true;
+
+
+
+    }
 
 
     system("pause");
     return 0;
 }
-
