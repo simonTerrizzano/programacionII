80b115fa04941e95afea8d010fc89da433ff2854
olivieri321 <83927968+olivieri321@users.noreply.github.com>
2023-06-19 15:59:19 -0300

    Add files via upload

---
+++ TP7_Conjuntos/funcionesConjuntos/funciones_conjuntos.c
@@ -0,0 +1,26 @@
+#include "conjuntos.h"
+#include "listas.h"
+#include "tipo_elemento.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "DataEntry.h"
+
+void randomizarConjunto(Conjunto* conjunto, int tamanio, int clavemax, int clavemin) {
+    TipoElemento X;
+    for (int i = 0; i < tamanio; i++) {
+        X = te_crear(rand() % (clavemax - clavemin + 1) + clavemin);
+        cto_agregar(*conjunto, X);
+    }
+}
+
+void crearConjunto(Conjunto* Conjunto){
+    TipoElemento X;
+    bool * res;
+    printf("Tamanio del conjunto:\n");
+    int tamanio = ingreso_normalizado_enteros(1, 1000,res);
+    for (int i = 0; i < tamanio + 1; i++) {
+        X = te_crear(ingreso_normalizado_enteros(-1000,1000,res));
+        cto_agregar(*Conjunto, X);
+    }
+}
